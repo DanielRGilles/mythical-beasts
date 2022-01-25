90f66c5c50725012adb641636148f1b6585bbc01
DanielRGilles <danryangilles@gmail.com>
2022-01-24 16:44:39 -0800

    initial commit

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS creatures;
+
+CREATE TABLE creatures (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title TEXT NOT NULL,
+  class TEXT NOT NULL,
+  lifespan TEXT NOT NULL,
+  environment TEXT NOT NULL,
+  img_url TEXT NOT NULL
+);
+
