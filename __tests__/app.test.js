ba1a80027d7540ab0e831aa606222f363ebf3131
DanielRGilles <danryangilles@gmail.com>
2022-01-24 17:53:08 -0800

    capitalization!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Creature = require('../lib/models/creature');
+const Creature = require('../lib/models/Creature');
 
 describe('refactory routes', () => {
   beforeEach(() => {
