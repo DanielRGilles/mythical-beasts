f73877d51b3188a1e314200ff0e1982d3a29c351
DanielRGilles <danryangilles@gmail.com>
2022-01-24 17:38:52 -0800

    testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,24 +16,23 @@ describe('refactory routes', () => {
   it('should be able to create a creature', async () => {
     const res = await Creature.insert({ title:'Falcore', species:'Dogon', lifespan:'not long enough', environment:'the-sky', img_url:'https://static.wikia.nocookie.net/theneverendingstory/images/8/8a/Falkor-neverending-story-96.2.jpg/revision/latest?cb=20190524234115' });
 
-    expect(res).toEqual({ title:'Falcore', species:'Dogon', lifespan:'not long enough', environment:'the-sky', img_url:'https://static.wikia.nocookie.net/theneverendingstory/images/8/8a/Falkor-neverending-story-96.2.jpg/revision/latest?cb=20190524234115' });
+    expect(res).toEqual({ id: expect.any(String), title:'Falcore', species:'Dogon', lifespan:'not long enough', environment:'the-sky', img_url:'https://static.wikia.nocookie.net/theneverendingstory/images/8/8a/Falkor-neverending-story-96.2.jpg/revision/latest?cb=20190524234115' });
   });
 
   it('should be able to list an creatures by id', async () => {
-    const creatures = await Creature.insert({ title:'', species:'', lifespan:'', environment:'', img_url:'' });
+    const creatures = await Creature.insert({ title:'Falcore', species:'Dogon', lifespan:'not long enough', environment:'the-sky', img_url:'https://static.wikia.nocookie.net/theneverendingstory/images/8/8a/Falkor-neverending-story-96.2.jpg/revision/latest?cb=20190524234115' });
     const res = await request(app).get(`/api/v1/creatures/${creatures.id}`);
     expect(res.body).toEqual(creatures);
   });
 
   it('should be able to list creatures', async () => {
-    await Creature.insert({ product: 'Widget', quantity: 1 });
+    await Creature.insert({  title:'Falcore', species:'Dogon', lifespan:'not long enough', environment:'the-sky', img_url:'https://static.wikia.nocookie.net/theneverendingstory/images/8/8a/Falkor-neverending-story-96.2.jpg/revision/latest?cb=20190524234115' });
     const res = await request(app).get('/api/v1/creatures');
 
     expect(res.body).toEqual([
       {
         id: expect.any(String),
-        title:'', species:'', lifespan:'', environment:'', img_url:'' 
-      },
+        title:'Falcore', species:'Dogon', lifespan:'not long enough', environment:'the-sky', img_url:'https://static.wikia.nocookie.net/theneverendingstory/images/8/8a/Falkor-neverending-story-96.2.jpg/revision/latest?cb=20190524234115' },
     ]);
   });
 
